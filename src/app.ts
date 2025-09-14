a76a7bc8d74ca91b6aaf6e91f3d755fe6b8efa30
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2025-09-13 18:09:42 -0700

    setting up errorhandling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
-import express from "express"
-// import {   } from "express" 
+import express from "express";
+import logger from "./config/logger";
+import { Request, Response, NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
 
-// import  from "express/lib/request.js";
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Auth Service is up and running")
-})
+app.get("/", (req: Request, res: Response) => {
+    const error = createHttpError(418, "This is a custom error message");
+    throw error;
+    res.send("Auth Service is up and running");
+});
+
+// Error handling middleware
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.status || 500;
+    
+    res.status(statusCode).json({
+        message: err.message,
+        type: err.name || "Internal Server Error",
+        path: req.path,
+        status: statusCode
+    });
+});
+
 export default app;
