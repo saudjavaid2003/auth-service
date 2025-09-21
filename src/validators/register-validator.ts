8a8f17ea23b05bcb347008f05fe0010acc7d8856
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2025-09-20 21:23:06 -0700

    moving to authenticatin

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,8 +1,30 @@
-import {body, checkSchema} from "express-validator"; 
+import { body, checkSchema } from "express-validator";
+
 export default checkSchema({
-    email:{
-        errorMessage:"where is your email",
-        notEmpty:true,
-        trim:true,
-    }
-})
+    firstName: {
+        errorMessage: "First name is required",
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: "Last name is required",
+        notEmpty: true,
+        trim: true,
+    },
+    email: {
+        errorMessage: "Email is required",
+        notEmpty: true,
+        trim: true,
+        isEmail: {
+            errorMessage: "Email must be a valid email address",
+        },
+    },
+    password: {
+        errorMessage: "Password is required",
+        notEmpty: true,
+        isLength: {
+            options: { min: 8 },
+            errorMessage: "Password must be at least 8 characters long",
+        },
+    },
+});
