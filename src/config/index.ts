9fdef33a5c01a4dbe37849477e248174d63e8b89
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2025-12-18 02:22:25 -0800

    migration done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,39 @@
 import { config } from "dotenv";
 import path from "path";
 
-const envPath = path.join(__dirname, "../../.env.dev");
-console.log("Loading from:", envPath);
+const NODE_ENV = process.env.NODE_ENV || "dev";
+
+// Direct mapping (simple & explicit – teacher style)
+const envPath = path.resolve(
+  __dirname,
+  "../../.env." + NODE_ENV
+);
+
+console.log("Loading env from:", envPath);
 
 config({ path: envPath });
 
-const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, REFRESH_TOKEN_SECRET, JWKS_URI } = process.env;
-
-console.log("DB_PASSWORD loaded:", DB_PASSWORD ? "YES" : "NO");
+const {
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  REFRESH_TOKEN_SECRET,
+  JWKS_URI,
+  
+} = process.env;
 
 export const Config = {
-    NODE_ENV,
-    PORT,
-    DB_HOST,
-    DB_PORT,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-    REFRESH_TOKEN_SECRET,
-    JWKS_URI,
+  NODE_ENV,
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  REFRESH_TOKEN_SECRET,
+  JWKS_URI,
+ 
 };
