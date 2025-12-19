acb3e44cc63db641d91daaba64d92be815557e92
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2025-12-18 22:18:16 -0800

    acessmiddleware

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -5,14 +5,17 @@ import { Request, Response ,NextFunction} from "express";
 import { AppDataSource } from "../config/data-source";
 import { Tenant } from "../entity/Tenant";
 import logger from "../config/logger";
+import { Roles } from "../constants";
 import { TenantService } from "../services/TenantService";
+import authenticate from "@/middlewares/authenticate";
+import { canAccess } from "@/middlewares/canAccess";
 const router = express.Router();
 const tenantRepository=AppDataSource.getRepository(Tenant)
 const tenantService=new TenantService(tenantRepository)
 
 const tenantController=new TenantController(tenantService,logger)
 
-router.post("/",(req:Request,res:Response,next:NextFunction)=>{
+router.post("/",authenticate,canAccess([Roles.ADMIN]),(req:Request,res:Response,next:NextFunction)=>{
     tenantController.create(req,res,next)
 });
 export default router;
