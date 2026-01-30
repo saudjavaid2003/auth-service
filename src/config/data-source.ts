c112481d579dfa99465ee48e2b8f95b293d5a405
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-01-30 03:21:41 -0800

    ready for production

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,18 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "../entity/User"
-import { Config } from "."
-import { RefreshToken } from "../entity/refreshToken"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import path from "path";
+import { Config } from ".";
+
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: Config.DB_HOST,
-    port: Number(Config.DB_PORT),
-    username: Config.DB_USERNAME,
-    password: Config.DB_PASSWORD,
-    database: Config.DB_NAME,
-    synchronize: false,
-    logging: false,
-    entities: ["src/entity/*.{ts,js}"],
-    migrations: ["src/migration/*.{ts,js}"],
-    subscribers: [],
-})
+  type: "postgres",
+  host: Config.DB_HOST,
+  port: Number(Config.DB_PORT),
+  username: Config.DB_USERNAME,
+  password: Config.DB_PASSWORD,
+  database: Config.DB_NAME,
+  synchronize: false,
+  logging: false,
+
+  entities: [path.join(__dirname, "../entity/*.{ts,js}")],
+  migrations: [path.join(__dirname, "../migration/*.{ts,js}")],
+
+  subscribers: [],
+});
